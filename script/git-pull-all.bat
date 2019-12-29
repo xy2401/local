17e9779ad0bccb8664ecdda06b07dbf3da25ce21
xy2401 <xy2401@163.com>
2019-12-29 14:35:55 +0800

    git pull push

---
+++ script/git-pull-all.bat
@@ -0,0 +1,33 @@
+  
+
+								 
+git -C ../../local                                         pull  
+git -C ../../local-doc-books                               pull  
+git -C ../../local-doc-help.eclipse.org                    pull  
+git -C ../../local-doc-help.eclipse.org-2019-06            pull  
+git -C ../../local-doc-help.eclipse.org-2019-06.zh         pull  
+git -C ../../local-doc-help.eclipse.org-tips               pull  
+git -C ../../local-doc-java-api                            pull  
+git -C ../../local-doc-oracle-11g                          pull  
+git -C ../../local-doc-oracle-12c                          pull  
+git -C ../../local-doc-oracle-18c                          pull  
+git -C ../../local-doc-oracle-19c                          pull  
+git -C ../../local-doc-oracle-19c.zh                       pull  
+git -C ../../local-docs                                    pull  
+git -C ../../local-docs-patch                              pull  
+git -C ../../local-site-201910-spring.io                   pull  
+git -C ../../local-site-201910-spring.io.zh                pull  
+git -C ../../local-site-201911-iata.org                    pull  
+git -C ../../local-site-201911-iata.org.pdf                pull  
+git -C ../../local-site-201911-iata.org.zh                 pull  
+git -C ../../local-site-201911-jcp.org                     pull  
+git -C ../../local-site-201911-jcp.org.zh                  pull  
+git -C ../../local-site-201912-docs.docker.com             pull  
+git -C ../../local-site-201912-docs.docker.com.zh          pull  
+git -C ../../local-site-201912-jetbrains.com               pull  
+git -C ../../local-site-201912-jetbrains.com.zh            pull  
+git -C ../../local-site-201912-openjdk.java.net            pull  
+git -C ../../local-site-201912-openjdk.java.net.zh         pull  
+git -C ../../local-web-util                                pull  
+git -C ../../local-youtube                                 pull  
+git -C ../../local-youtube-vtt                             pull  
---
+++ script/git-push-all.bat
@@ -0,0 +1,33 @@
+  
+
+								 
+git -C ../../local                                         push  
+git -C ../../local-doc-books                               push  
+git -C ../../local-doc-help.eclipse.org                    push  
+git -C ../../local-doc-help.eclipse.org-2019-06            push  
+git -C ../../local-doc-help.eclipse.org-2019-06.zh         push  
+git -C ../../local-doc-help.eclipse.org-tips               push  
+git -C ../../local-doc-java-api                            push  
+git -C ../../local-doc-oracle-11g                          push  
+git -C ../../local-doc-oracle-12c                          push  
+git -C ../../local-doc-oracle-18c                          push  
+git -C ../../local-doc-oracle-19c                          push  
+git -C ../../local-doc-oracle-19c.zh                       push  
+git -C ../../local-docs                                    push  
+git -C ../../local-docs-patch                              push  
+git -C ../../local-site-201910-spring.io                   push  
+git -C ../../local-site-201910-spring.io.zh                push  
+git -C ../../local-site-201911-iata.org                    push  
+git -C ../../local-site-201911-iata.org.pdf                push  
+git -C ../../local-site-201911-iata.org.zh                 push  
+git -C ../../local-site-201911-jcp.org                     push  
+git -C ../../local-site-201911-jcp.org.zh                  push  
+git -C ../../local-site-201912-docs.docker.com             push  
+git -C ../../local-site-201912-docs.docker.com.zh          push  
+git -C ../../local-site-201912-jetbrains.com               push  
+git -C ../../local-site-201912-jetbrains.com.zh            push  
+git -C ../../local-site-201912-openjdk.java.net            push  
+git -C ../../local-site-201912-openjdk.java.net.zh         push  
+git -C ../../local-web-util                                push  
+git -C ../../local-youtube                                 push  
+git -C ../../local-youtube-vtt                             push  
